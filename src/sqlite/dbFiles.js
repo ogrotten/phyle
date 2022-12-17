7a88f125d5b0663db4a7c720f6539022e385b768
monsto <ogrotten@users.noreply.github.com>
2022-12-17 17:47:40 -0600

    woo

--- src/sqlite/dbFiles.js
+++ src/sqlite/dbFiles.js
@@ -5,19 +5,27 @@ exports.storeFiles = async (incoming) => {
 	// console.log(`conlog: incoming`, incoming)
 
 	let values = ''
-	incoming.forEach((item) => {
-		// console.log(`conlog: item`, item)
-		values += `(${item.name},${item.path},${item}),`
-	})
+	let info = []
 
-	values = values.slice(0, -1)
+	const stmt = db.prepare(
+		`INSERT INTO files(filename, path, object) VALUES(?, ?, ?)`
+	)
+	incoming.forEach((item) => {
+		console.log(`conlog: item`, item)
+		// values += `(${item.name},${item.path},${item}),`
+		const res = stmt.run(item.name, item.path, JSON.stringify(item))
+		info.push(res)
+	})
+	console.log(`conlog: info`, info)
+
+	// values = values.slice(0, -1)
 
 	// const outgoing = db.exec(
 	// 	`INSERT INTO files(filename, path) VALUES("Name of files", "Path of files")`
 	// )
 	// console.log(`conlog: outgoing`, outgoing)
 
-	const stmt = db.prepare(`INSERT INTO files(filename, path) VALUES(?, ?)`)
-	const info = stmt.run('Joey', 2)
-	console.log(`conlog: info`, info)
+	// const stmt = db.prepare(`INSERT INTO files(filename, path) VALUES(?, ?)`)
+	// const info = stmt.run('Joey', 2)
+	// console.log(`conlog: info`, info)
 }

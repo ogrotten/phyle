71ab3cd1cf6021e05c02aea25a855a703939100a
monsto <ogrotten@users.noreply.github.com>
2022-12-17 20:27:29 -0600

    layout fixed

--- src/renderer/src/components/Filebrowse.svelte
+++ src/renderer/src/components/Filebrowse.svelte
@@ -42,8 +42,11 @@
 </script>
 
 <div class="bg-base-100 card m-4 p-4 h-96">
-	<div class="flex gap-4 h-full">
-		<div class="space-y-2 flex flex-col">
+	<div
+		id="Container"
+		class="m-auto flex h-full w-full justify-between gap-8 bg-blue-400"
+	>
+		<div id="Drives" class="h-full w-1/6 bg-green-400">
 			{#each volumes as drive, idx}
 				<button
 					class="btn btn-sm btn-circle btn-info"
@@ -53,59 +56,69 @@
 				</button>
 			{/each}
 		</div>
-		<div class="">
-			<div class="w-full border-b border-grey-200 pb-2 mb-2">
-				<p class="">{cwd}</p>
+		<div class="h-full w-1/6 grow bg-violet-400 p-4 flex flex-col gap-4">
+			<div class="bg-green-200">
+				<div class="w-full border-b border-grey-200 pb-2 mb-2">
+					<p class="">{cwd}</p>
+				</div>
 			</div>
-			{#await filesPromise then files}
+			<div
+				on:click={() => (cwd = '/')}
+				class="font-semibold text-primary-focus cursor-pointer hover:text primary hover:bg-neutral-content p-1 w-full"
+			>
+				[root]
+			</div>
+			{#if !isRoot}
 				<div
-					on:click={() => (cwd = '/')}
+					on:click={() => navigateUp()}
 					class="font-semibold text-primary-focus cursor-pointer hover:text primary hover:bg-neutral-content p-1 w-full"
 				>
-					[root]
+					[up]
 				</div>
-				{#if !isRoot}
-					<div
-						on:click={() => navigateUp()}
-						class="font-semibold text-primary-focus cursor-pointer hover:text primary hover:bg-neutral-content p-1 w-full"
-					>
-						[up]
-					</div>
-				{/if}
-				<div class=" h-full overflow-auto">
-					<ul class="ml-2">
-						{#each files.sort((a, b) => a.type.localeCompare(b.type)) as entry}
-							{#if entry.type === 'directory'}
-								<li
-									class="cursor-pointer hover:bg-neutral-content p-1 w-full hover:text-primary"
-									on:click={() => navigate(entry.name)}
-								>
-									<span
-										href={''}
-										class="font-semibold text-primary-focus p-1 w-full"
+			{/if}
+			<div class="overflow-y-auto overflow-x-hidden bg-yellow-200">
+				{#await filesPromise then files}
+					<div class=" h-full overflow-auto">
+						<ul class="ml-2">
+							{#each files.sort( (a, b) => a.type.localeCompare(b.type) ) as entry}
+								{#if entry.type === 'directory'}
+									<li
+										class="cursor-pointer hover:bg-neutral-content p-1 w-full hover:text-primary"
+										on:click={() => navigate(entry.name)}
 									>
-										/{entry.name}
-									</span>
-								</li>
-							{:else}
-								<li>
-									<p class=" p-1 text-neutral-focus">{entry.name}</p>
-								</li>
-							{/if}
-						{/each}
-					</ul>
-				</div>
-			{/await}
+										<span
+											href={''}
+											class="font-semibold text-primary-focus p-1 w-full"
+										>
+											/{entry.name}
+										</span>
+									</li>
+								{:else}
+									<li>
+										<p class=" p-1 text-neutral-focus">{entry.name}</p>
+									</li>
+								{/if}
+							{/each}
+						</ul>
+					</div>
+				{/await}
+			</div>
 		</div>
+		<div class="h-full w-1/6 bg-amber-400" />
 	</div>
 </div>
+
 <div class="flex justify-end">
-	<button class="btn btn-primary btn-sm" on:click={search}>
+	<button class="btn btn-primary btn-sm" disabled on:click={search}>
 		Choose Directory
 	</button>
 </div>
 
 <style lang="postcss">
+	.dev * {
+		@apply border border-red-500;
+	}
+
 	/* li {
 		@apply m-0;
 	}

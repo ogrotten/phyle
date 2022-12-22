a07ba6c5e2e5d933251aa34d98c0f005fa2b9a8b
monsto <ogrotten@users.noreply.github.com>
2022-12-21 23:13:04 -0600

    auto pulling the tag ids

--- src/sqlite/dbTags.js
+++ src/sqlite/dbTags.js
@@ -1,7 +1,7 @@
 const dbmgr = require('./dbmgr')
 const db = dbmgr.db
 
-const allTags = db.prepare('SELECT * FROM tags').all()
+const allTags = db.prepare('SELECT rowid,name,data,type FROM tags').all()
 
 exports.autoTag = async (incoming) => {
 	if (!incoming) return
@@ -10,8 +10,21 @@ exports.autoTag = async (incoming) => {
 
 	incoming.forEach((e) => {
 		e.tags_auto = ' '
-		const hold = e.name.match(toMatch)
-		if (hold) e.tags_auto = Array.from(hold).join(',')
+
+		const hold = e.name.toLowerCase().match(toMatch)
+		if (!hold) return
+
+		// console.log(`conlog: hold`, hold)
+		const tagIds = allTags
+			.filter((tag) => {
+				console.log(`conlog: hold`, hold, tag.data)
+				return hold?.includes(tag.data)
+			})
+			.map((e) => e.rowid)
+
+		console.log(`conlog: tagIds`, tagIds)
+
+		e.tags_auto = Array.from(hold).join(',')
 	})
 
 	return incoming

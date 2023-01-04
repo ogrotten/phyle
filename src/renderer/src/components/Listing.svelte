141e177a47555c25c82d0bdbff2f2435db418e6c
monsto <ogrotten@users.noreply.github.com>
2023-01-04 01:03:57 -0600

    more or less filter works.

--- src/renderer/src/components/Listing.svelte
+++ src/renderer/src/components/Listing.svelte
@@ -51,8 +51,11 @@
 
 	// 	return filterTags.length > 0 ? intersection : files
 	// }
+	$: console.log(`conlog: filterFiles`, filterFiles)
 
-	const moreFilter = async (idx) => {
+	const moreFilter = async (tag) => {
+		const idx = allTags.findIndex((e) => e.id === tag.id)
+
 		allTags[idx].in = true
 		filterTags.push(allTags[idx].data)
 		filterFiles = await window.api.dbFiles.filterFiles(filterTags)
@@ -64,7 +67,7 @@
 		allTags[idx].in = false
 		filterTags = filterTags.filter((e) => e != allTags[idx].data)
 		if (filterTags?.length < 1) filterFiles = await window.api.getFiles()
-		else filterFiles = window.api.dbFiles.filterFiles(filterTags)
+		else filterFiles = await window.api.dbFiles.filterFiles(filterTags)
 	}
 
 	onMount(async () => {
@@ -81,11 +84,11 @@
 <div class="card w-full h-full grow flex flex-col  pr-0">
 	<div class="border-b-4 border-neutral-content h-48 mb-4">
 		<div class="flex">
-			{#each headerTags as tag, idx (tag.id)}
+			{#each headerTags as tag (tag.id)}
 				<p
 					transition:fade={{ duration: 100 }}
 					class="tag cursor-pointer"
-					on:click={() => moreFilter(idx)}
+					on:click={() => moreFilter(tag)}
 				>
 					{tag.tag}
 				</p>
